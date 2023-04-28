188d4852a0df22b3f147ba0899f8e186462431fb
yuyefeng3574 <whshi574@gmail.com>
2023-04-28 17:04:40 +0800

    4.28

---
+++ Source/Main/MainGameModeBase.h
@@ -0,0 +1,17 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/GameModeBase.h"
+#include "MainGameModeBase.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class MAIN_API AMainGameModeBase : public AGameModeBase
+{
+	GENERATED_BODY()
+	
+};
